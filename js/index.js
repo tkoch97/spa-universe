64faf5bba3005a7b508699f86669e2c2a770af43
Thiago Koch <tkoch.gim@gmail.com>
2023-02-08 19:23:59 -0300

    feat: add SPA functions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 import { Events } from "./events.js";
+import { Router } from "./routes.js";
+
+const router = new Router();
+router.add("/", "/pages/home.html")
+router.add("/the-universe", "/pages/the-universe.html")
+router.add("/explorer", "/pages/explorer.html")
 
 Events();
+router.handle()
+window.onpopstate = () => router.handle()
