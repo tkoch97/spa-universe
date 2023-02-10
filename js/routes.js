1537e341c55482f08f0cbf45f2fc2d1bf75ac5d9
Thiago Koch <tkoch.gim@gmail.com>
2023-02-10 10:00:08 -0300

    docs: add-coment

--- js/routes.js
+++ js/routes.js
@@ -39,3 +39,23 @@ export class Router {
   }
   
 }
+
+/* 
+Esse é o código de uma classe "Router" que gerencia a navegação entre diferentes páginas em uma aplicação. 
+Ele armazena as rotas como uma propriedade "routes" como um objeto onde as chaves são os nomes de rota e os valores são as páginas correspondentes.
+
+A classe possui os seguintes métodos:
+
+"add": adiciona uma nova rota ao objeto "routes".
+
+"route": recebe um evento como argumento (ou usa um evento padrão) e impede a ação padrão do evento, 
+adiciona uma nova entrada ao histórico do navegador e chama o método "handle".
+
+"handle": obtém o pathname da localização atual, busca a rota correspondente no objeto "routes" e busca o HTML da página correspondente 
+usando o fetch API. O conteúdo HTML é então inserido no elemento HTML com id "app". O método "changeImageAndMarkdown" é chamado ao final.
+
+"changeImageAndMarkdown": altera a imagem de fundo do body e a cor do link de acordo com a rota atual.
+
+A classe Router é usada para gerenciar a navegação entre páginas na aplicação e atualizar o conteúdo da 
+página e a imagem de fundo de acordo com a rota atual.
+*/
