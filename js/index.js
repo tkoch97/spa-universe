cf95d62e57cef41e18f4ea39e623f230448c0ae9
Thiago Koch <tkoch.gim@gmail.com>
2023-02-10 09:55:42 -0300

    feat: add-window.route

--- js/index.js
+++ js/index.js
@@ -9,3 +9,11 @@ router.add("/explorer", "./pages/explorer.html")
 Events();
 router.handle()
 window.onpopstate = () => router.handle()
+window.route = () => router.route();
+
+/* 
+se a linha "window.route = () => router.route();" for removida e se a aplicação depende dela para navegar entre páginas, 
+ao clicar em um dos links que deveriam mudar a página, é possível que ocorra um erro 404 (página não encontrada). 
+Isso acontece porque a função "route" não estará mais disponível para ser chamada e, portanto, 
+o método "route" da instância de Router não será executado para navegar entre páginas.
+*/
